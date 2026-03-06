7efd6de8ae8223362a4e6ec083d4f7a1f968ed88
Aditya <shukla.aditya2006@gmail.com>
2026-03-06 23:15:48 +0530

    feat: add flags

--- cmd/root.go
+++ cmd/root.go
@@ -1,53 +1,62 @@
-/*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+	/*
+	Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 
-*/
-package cmd
+	*/
+	package cmd
 
-import (
-	"os"
+	import (
+		"os"
+		"fmt"
+		"github.com/spf13/cobra"
+	)
 
-	"github.com/spf13/cobra"
-)
 
 
+	// rootCmd represents the base command when called without any subcommands
+	var rootCmd = &cobra.Command{
+		Use:   "hotreload",
+		Short: "A brief description of your application",
+		Long: `A longer description that spans multiple lines and likely contains
+	examples and usage of using your application. For example:
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "hotreload",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
-}
-
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
+	Cobra is a CLI library for Go that empowers applications.
+	This application is a tool to generate the needed files
+	to quickly create a Cobra application.`,
+		// Uncomment the following line if your bare application
+		// has an action associated with it:
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Root directory:", rootDir)
+			fmt.Println("Build command:", buildCmd)
+			fmt.Println("Exec command:", execCmd)
+		},
 	}
-}
-var rootDir string
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.hotreload.yaml)")
+	// Execute adds all child commands to the root command and sets flags appropriately.
+	// This is called by main.main(). It only needs to happen once to the rootCmd.
+	func Execute() {
+		err := rootCmd.Execute()
+		if err != nil {
+			os.Exit(1)
+		}
+	}
+	var rootDir string
+	var buildCmd string
+	var execCmd string
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	func init() {
+		// Here you will define your flags and configuration settings.
+		// Cobra supports persistent flags, which, if defined here,
+		// will be global for your application.
 
-	rootCmd.Flags().StringVar(&rootDir,"root",".","Directory to watch for file changes (including all subfolders)")
-}
+		// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.hotreload.yaml)")
+
+		// Cobra also supports local flags, which will only run
+		// when this action is called directly.
+		rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+		rootCmd.PersistentFlags().StringVarP(&rootDir,"root","r",".","Directory to watch for file changes (including all subfolders)")
+		rootCmd.PersistentFlags().StringVarP(&buildCmd,"build","b","","Command to build the application")
+		rootCmd.PersistentFlags().StringVarP(&execCmd,"exec","e","","Command to execute the application")
+	}
 
 
